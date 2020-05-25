56cbf39d16b6694bc56fb306715ef19599af7e5e
Darkimage <luc-af@live.it>
2020-05-25 13:28:27 +0200

    [PYTHON] Added class to websocket link module

--- megascan-link-python/websocket_link.py
+++ megascan-link-python/websocket_link.py
@@ -1,12 +1,16 @@
 from websocket import create_connection
 import PySide2
 from PySide2 import QtWidgets, QtGui, QtCore
+from . import log
+import json
 
+class WebsocketLink(QtCore.QObject):
+	def __init__(self, parent=None):
+		super().__init__(parent=parent)
 
-def sendDataToJs():
-	ws = create_connection("ws://localhost:1212/")
-	print("Sending 'Hello, World'...")
-	ws.send("Hello, World")
-	print("Sent")
-	ws.close()
+	def sendDataToJs(self, data: object):
+		ws = create_connection("ws://localhost:1212/")
+		log.LoggerLink.Log("Sending data to JS plugin")
+		ws.send(json.dumps(data))
+		ws.close()
 
